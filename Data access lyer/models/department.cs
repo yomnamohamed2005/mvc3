29f31d85cabc1e2807089a9c7d874f347b67d14a
MAHMOUD MOHAMED <MAHMOUD.89179@eng.mti.edu.eg>
2024-09-26 04:44:44 +0300

    first commit by me

--- Data access lyer/models/department.cs
+++ Data access lyer/models/department.cs
@@ -11,7 +11,9 @@ namespace Data_access_lyer.models
     public class department
     {
         public int id { get; set; }
+        [Range(0,500)]
         public int code { get; set; }
+        [Required(ErrorMessage ="Name is Requried !!")]
         public string name { get; set; }
         [Display(Name = "Create at")]
         public DateTime time { get; set; }
--- mvc3/Controllers/departmentController1.cs
+++ mvc3/Controllers/departmentController1.cs
@@ -1,12 +1,14 @@
 ﻿using Business_acess_lyer.repositories;
 using Microsoft.AspNetCore.Mvc;
 using Data_access_lyer.models;
+using Microsoft.Identity.Client;
+
 namespace mvc3.Controllers
 {
-    public class departmentController1 : Controller
+    public class departmentController : Controller
     {
         private readonly Idata_repositories datareposatory;
-        public departmentController1 (Idata_repositories _repo)
+        public departmentController(Idata_repositories _repo)
         {
             datareposatory = _repo;
         }
@@ -15,6 +17,135 @@ namespace mvc3.Controllers
         {
             var result = datareposatory.Getall();
             return View(result);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(department department)
+        {
+            if (!ModelState.IsValid) return View(department);
+
+
+            datareposatory.create(department);
+            return RedirectToAction(nameof(Index));
+
+
+        }
+        public IActionResult details(int? id)
+        {
+            if (id is null)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                var department = datareposatory.Get(id.Value);
+                if (department is null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(department);
+                }
+            }
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                var department = datareposatory.Get(id.Value);
+                if (department is null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(department);
+                }
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromRoute] int Id, department department)
+        {
+            if (Id != department.id) { return BadRequest(); }
+
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+
+                        datareposatory.update(department);
+                        return RedirectToAction(nameof(Index));
+
+                    }
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError("", ex.Message);
+                    }
+                }
+
+                return View(department);
+
+            }
+
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (!id.HasValue)
+            {
+                return BadRequest();
+            }
+            else
+            {
+                var department = datareposatory.Get(id.Value);
+                if (department is null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(department);
+                }
+
+            }
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult ConfirmDelete(int? id)
+        {
+
+            if (!id.HasValue) { return BadRequest(); }
+            var repo = datareposatory.Get(id.Value);
+            if (repo is null) { return NotFound(); }
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    try
+                    {
+
+                        datareposatory.delete(repo);
+                        return RedirectToAction(nameof(Index));
+
+                    }
+                    catch (Exception ex)
+                    {
+                        ModelState.AddModelError("", ex.Message);
+                    }
+                }
+
+                return View(repo);
+            }
+
         }
     }
 }
